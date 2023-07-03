253f177a343485245980f2ce56e140a7837f349a
dimmerz92 <andrew.weymes@outlook.com>
2023-07-03 23:02:50 +0800

    add method to get files

--- char_count.c
+++ char_count.c
@@ -5,26 +5,44 @@
 #include <string.h>
 
 int parse_args(int argc, char *argv[], int *nprocs, DIR **dir);
-//int get_files();
+void get_files(DIR *dir, char *files[]);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     DIR *dir;
-    struct dirent *entry;
     int nprocs;
+    char **files = NULL;
 
-    if (parse_args(argc, argv, &nprocs, &dir)) {
+    if (parse_args(argc, argv, &nprocs, &dir) < 0) {
         perror("Usage: char_count directory_name");
     }
-    while ((entry = readdir(dir)) != NULL){
-        printf("%s\n", entry->d_name);
-    }
+    
+    get_files(dir, files);
+
+    /*for (int n = 0; n < 2; n++) {
+        printf("%s\n", files[n]);
+    }*/
     closedir(dir);
+    free(files);
     return 0;
 }
 
-int parse_args(int argc, char *argv[], int *nprocs, DIR **dir){
+int parse_args(int argc, char *argv[], int *nprocs, DIR **dir) {
     if (argc != 3) return -1;
     if ((*nprocs = atoi(argv[1])) < 1) return -2;
     if ((*dir = opendir(argv[2])) == NULL) return -3;
     return 0;
+}
+
+void get_files(DIR *dir, char *files[]) {
+    int i = 0;
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") == 0 ||
+            strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        files = realloc(files, (i+1) * sizeof(char*));
+        files[i] = entry->d_name;
+    }
 }
