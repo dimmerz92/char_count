a04cf42d3e22b2aca03890b2794c05eb5b90694d
dimmerz92 <andrew.weymes@outlook.com>
2023-07-05 11:10:50 +0800

    add ring interprocess communication methods

--- char_count.c
+++ char_count.c
@@ -7,6 +7,8 @@
 
 int parse_args(int argc, char *argv[], int *nprocs, DIR **dir);
 int get_files(DIR *dir, char **files);
+int init_ring();
+int add_node(int *pid);
 
 int main(int argc, char *argv[]) {
     DIR *dir;
@@ -37,6 +39,7 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/* checks arg numbers, assigns nprocs and dir from command args */
 int parse_args(int argc, char *argv[], int *nprocs, DIR **dir) {
     if (argc != 3) return -1;
     if ((*nprocs = atoi(argv[1])) < 1) return -2;
@@ -44,6 +47,7 @@ int parse_args(int argc, char *argv[], int *nprocs, DIR **dir) {
     return 0;
 }
 
+/* uses regex to get all .txt files from dir arg and assigns to files arg */
 int get_files(DIR *dir, char **files) {
     int i = 0;
     regex_t regex;
@@ -62,6 +66,42 @@ int get_files(DIR *dir, char **files) {
 
     if (i == 0) {
         return -1;
+    }
+    return 0;
+}
+
+int init_ring() {
+    int fd[2];
+
+    if (pipe(fd) < 0) {
+        return -1;
+    }
+    if (dup2(fd[0], STDIN_FILENO) < 0 || dup2(fd[1], STDOUT_FILENO) < 0) {
+        return -2;
+    }
+    if (close(fd[0]) < 0 || close(fd[1]) < 0) {
+        return -3;
+    }
+    return 0;
+}
+
+int add_node(int *pid) {
+    int fd[2];
+
+    if (pipe(fd) < 0) {
+        return -1;
+    }
+    if ((*pid = fork()) < 0) {
+        return -2;
+    }
+    if (*pid > 0 && dup2(fd[1], STDOUT_FILENO) < 0) {
+        return -3;
+    }
+    if (*pid == 0 && dup2(fd[0], STDIN_FILENO) < 0) {
+        return -4;
+    }
+    if (close(fd[0]) < 0 || close(fd[1]) < 0) {
+        return -5;
     }
     return 0;
 }
