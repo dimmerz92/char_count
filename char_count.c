ce19b75b2ecc328fe521d1735205232992a5e850
dimmerz92 <andrew.weymes@outlook.com>
2023-07-08 16:31:10 +0800

    fix hist print function

--- char_count.c
+++ char_count.c
@@ -219,6 +219,7 @@ void print_hist(int *counts) {
     for (int i = 0; i < 26; i++) {
         bar_height = counts[i]*500.0/counts[26];
         fprintf(stderr, "Process 1 got char %c: %d\t", 'a' + i, counts[i]);
+        if (counts[i]) fprintf(stderr, "*");
         for (int j = 0; j < bar_height; j++) {
             fprintf(stderr, "*");
         }
